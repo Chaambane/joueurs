9eb7052fd104ddeaefee408b871aa545e4f840e2
FAIDINE <mohamed.faidine@live.fr>
2021-11-01 10:43:14 +0100

    feat(index) add google font

---
+++ src/containers/JoueursManager/JoueursManager.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class JoueursManager extends Component {
+    render() {
+        return (
+            <h1 className="container bg-info">Créateur de personnage</h1>
+        )
+    };
+}
+
+export default JoueursManager;
