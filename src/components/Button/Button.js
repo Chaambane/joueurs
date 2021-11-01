173898f6ba4e9fdd5c299004dd0afd022fd619bd
FAIDINE <mohamed.faidine@live.fr>
2021-11-01 14:25:32 +0100

    refact(button) add width

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const button = (props) => {
-    const cssBtn = `btn ${props.clrBtn} fw-bold me-1`;
+    const cssBtn = `btn ${props.clrBtn} fw-bold w-100`;
     return(
         <button className={cssBtn} onClick={props.clic}>
             {props.children}
